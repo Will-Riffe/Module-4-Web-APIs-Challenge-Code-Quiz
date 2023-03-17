32e4b1f4b99075987a418127eed7fc013221bfb7
Will Riffe <w_riffe@protonmail.com>
2023-03-17 09:37:17 -0400

    added js const rel to index ; added q's/a's

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+//Defining Variables
+const quizContainer = document.querySelector(".quiz");
+const questionElement = document.querySelector(".question");
+const choicesElement = document.querySelector(".choices");
+
+// Define the quiz questions and answer choices
+const quizData = [
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyper Text Markup Language", "Hyperlink Text Mark Language"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "What does CSS stand for?",
+        choices: ["Cascading Style Sheets", "Computer Style Sheets", "Colorful Style Sheets", "Creative Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "What does JS stand for?",
+        choices: ["Java Style", "JavaScript", "Java Source", "Just Saying"],
+        answer: "JavaScript"
+    }
+];
